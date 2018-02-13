4deb6907e39a9f3dee0ef2da2f4c2123caf54bd3
Matthew Conrad <matthew.conrad@outlook.com>
2018-02-12 23:36:17 -0800

    Adjusted printName1 and printName2

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
   "use strict";
 
-  // Globals
+  // Variables
   const startScreen = document.getElementById("start"),
     startButton = document.querySelector(".button"),
     newButton = document.getElementById("newGame"), 
@@ -12,42 +12,77 @@
     square = document.querySelector(".boxes > .box"),
     o = "url(img/o.svg)",
     x = "url(img/x.svg)",
+    onePlayer = document.getElementById("onePlayer"),
+    twoPlayers = document.getElementById("twoPlayers"),
+    player1Div = document.getElementById("playerOne"),
+    player2Div = document.getElementById("playerTwo"),
     board = [
       [1, 2, 3], // [0][0], [0][1], [0][2]
       [4, 5, 6], // [1][0], [1][1], [1][2]
       [7, 8, 9]  // [2][0], [2][1], [2][2]
-    ],
-    onePlayer = document.getElementById("onePlayer"),
-    twoPlayers = document.getElementById("twoPlayers"),
-    player1Div = document.getElementById("playerOne"),
-    player2Div = document.getElementById("playerTwo");
-  // Locals
+    ];
+
   let player1 = document.getElementById("player1"),
     player2 = document.getElementById("player2"),
-    printName1 = document.getElementById("printName1"),
-    printName2 = document.getElementById("printName2"),
     player1Name = document.getElementById("player1Name"),
     player2Name = document.getElementById("player2Name"),
+    printName1 = document.getElementById("printName1"),
+    printName2 = document.getElementById("printName2"),
     currentPlayer = player1,
-    player1Move = [],
-    player2Move = [],
     wins = [
-      [1, 2, 3],
+      [1, 2, 3], // Horizontal
       [4, 5, 6],
       [7, 8, 9],
-      [1, 4, 7],
+      [1, 4, 7], // Vertical
       [2, 5, 8],
       [3, 6, 9],
-      [1, 5, 9],
-      [3, 5, 7],
-    ];
+      [1, 5, 9], // Diagonal
+      [3, 5, 7]
+    ],
+    player1Move = [],
+    player2Move = [];
+
+  // Start Screen
+  function gameStart() {
+    startScreen.style.display = "block";
+    boardScreen.style.display = "none";
+    finishScreen.style.display = "none";
+  }
+
+  // Board Screen
+  function newGame() {
+    let name1 = player1Name.value,
+      name2 = player2Name.value;
+    startScreen.style.display = "none";
+    boardScreen.style.display = "block";
+    finishScreen.style.display = "none";
+    player1Move = [];
+    player2Move = [];
+    if (name1 == "") {
+      printName1.innerHTML = "You";
+      printName2.innerHTML = "Computer";
+    } else if (name2 == "") {
+      printName1.innerHTML = name1;
+      printName2.innerHTML = "Computer";
+    } else {
+      printName1.innerHTML = name1;
+      printName2.innerHTML = name2;  
+    }
+  }
+
+  // Finish Screen
+  function endScreen() {
+    startScreen.style.display = "none";
+    boardScreen.style.display = "none";
+    finishScreen.style.display = "block";
+  }
 
   // Number of Players
   function selectPlayers() {
-    playerOne.style.display = "none";
-    playerTwo.style.display = "none";
     let players = document.forms[0],
       i;
+    playerOne.style.display = "none";
+    playerTwo.style.display = "none";
     for (i = 0; i < players.length; i++) {
       players.addEventListener("click", () => {
         if (players[0].checked) {
@@ -61,44 +96,8 @@
           playerTwo.style.display = "none";
         }
       });
-      startButton.addEventListener("click", () => {
-        let name1 = player1Name.value,
-          name2 = player2Name.value;
-        if (players[0].checked) {
-          printName1.innerHTML = name1;
-          printName2.innerHTML = "Computer";
-        } else if (players[1].checked) {
-          printName1.innerHTML = name1;
-          printName2.innerHTML = name2;
-        }
-      });
     }
   }
-
-  // Start Screen
-  function gameStart() {
-    startScreen.style.display = "block";
-    boardScreen.style.display = "none";
-    finishScreen.style.display = "none";
-  }
-
-  // Board Screen
-  function newGame() {
-    startScreen.style.display = "none";
-    boardScreen.style.display = "block";
-    finishScreen.style.display = "none";
-    player1Move = [];
-    player2Move = [];
-  }
-
-  // Finish Screen
-  function endScreen() {
-    startScreen.style.display = "none";
-    boardScreen.style.display = "none";
-    finishScreen.style.display = "block";
-  }
-
-  startButton.addEventListener("click", newGame);
 
   // Random Player
   function pickRandom() {
@@ -140,73 +139,25 @@
     });
   }
 
-  // Mark Square
+  // Switch Player
   function switchPlayer() {
     currentPlayer = currentPlayer == player1 ? player2 : player1;
   }
 
-  function pickSquare() {
-    let square = board,
-      player;
-
-    if (currentPlayer == player1) {
-      player = player1Move;
-    } else {
-      player = player2Move;
-    }
-
-    squares.addEventListener("click", (e) => {
-      if (currentPlayer == player1) {
-        e.target.classList.add("box-filled-1");
-        player1Move.push("O");
-        switchPlayer();
-        unhighlightPlayer();
-        highlightPlayer();
-        e.target.style.pointerEvents ="none";
-        console.log(player1Move);
-      } else {
-        e.target.classList.add("box-filled-2");
-        player2Move.push("X");
-        switchPlayer();
-        unhighlightPlayer();
-        highlightPlayer();
-        e.target.style.pointerEvents ="none";
-        console.log(player2Move);
-      }
-      if (player1Move.length + player2Move.length === 9) {
-        let message = document.querySelector(".message");
-        message.innerHTML = "It's a Tie!";
-        endScreen();
-        finishScreen.classList.remove("screen-win-one");
-        finishScreen.classList.remove("screen-win-two");
-        finishScreen.classList.add("screen-win-tie");
-      }
-    });
-  }
-
   // Check for Win
   function checkWin() {
-    let square = board,
-      player;
-
+    let player;
     if (currentPlayer == player1) {
       player = player1Move;
     } else {
       player = player2Move;
     }
-
-    // square.forEach((el, index, array) => {
-    //   el.forEach((el2d, index2d, array2d) => {
-    //       console.log(square[0][2]);  // Logs 3 to the Console
-    //   });
-    // });
 
     for (let i = 0; i < wins.length; i++) {
       let win = wins[i];
       for (let j = 0; j < win.length; j++) {
         let num = win[j],
           compare = player.indexOf(num);
-          // console.log(compare); // Logs -1 to the Console
 
         if (compare === -1) {
           return true;
@@ -215,26 +166,55 @@
     }
   }
 
+  // Pick Square
+  function pickSquare() {
+    let winner = checkWin(),
+      message = document.querySelector(".message");
+
+    squares.addEventListener("click", (e) => {
+      if (currentPlayer == player1) {
+        // player1Move += 
+        e.target.classList.add("box-filled-1");
+        switchPlayer();
+        unhighlightPlayer();
+        highlightPlayer();
+        e.target.style.pointerEvents ="none";
+      } else {
+        // player2Move += 
+        e.target.classList.add("box-filled-2");
+        switchPlayer();
+        unhighlightPlayer();
+        highlightPlayer();
+        e.target.style.pointerEvents ="none";
+      }
+
+      if (winner) {
+        if (winner == player1) {
+        message.innerHTML = printName1.textContent + " Wins";
+        endScreen();
+        finishScreen.classList.add("screen-win-one");
+        finishScreen.classList.remove("screen-win-two");
+        finishScreen.classList.remove("screen-win-tie");
+        } else if (winner == player2) {
+        message.innerHTML = printName2.textContent + " Wins";
+        endScreen();
+        finishScreen.classList.remove("screen-win-one");
+        finishScreen.classList.add("screen-win-two");
+        finishScreen.classList.remove("screen-win-tie");
+        } else if (player1Move.length + player2Move.length === 9) {
+        message.innerHTML = "It's a Tie!";
+        endScreen();
+        finishScreen.classList.remove("screen-win-one");
+        finishScreen.classList.remove("screen-win-two");
+        finishScreen.classList.add("screen-win-tie");
+        }
+      }
+    });
+  }
+
   // AI
   function AI() {
 
-  }
-
-  // Game Over
-  function gameOver() {
-    let winner = checkWin(),
-      message = document.querySelector(".message");
-    if (winner == player1) {
-      message.innerHTML = "Wins";
-      finishScreen.classList.add("screen-win-one");
-      finishScreen.classList.remove("screen-win-two");
-      finishScreen.classList.remove("screen-win-tie");
-    } else if (winner == player2) {
-      message.innerHTML = "Wins";
-      finishScreen.classList.remove("screen-win-one");
-      finishScreen.classList.add("screen-win-two");
-      finishScreen.classList.remove("screen-win-tie");
-    }
   }
 
   function reset() {
@@ -242,6 +222,7 @@
   }
 
   // Gameplay
+  startButton.addEventListener("click", newGame);
   gameStart();
   selectPlayers();
   // AI();
@@ -250,7 +231,6 @@
   highlightSquare();
   unhighlightSquare();
   pickSquare();
-  // checkWin();
   newButton.addEventListener("click", reset);
 
 })();
